0889368178282bb7ddaa3c5408e337f517f9e63f
Mary Lee <mary.lee.ny@gmail.com>
2020-11-20 20:47:04 -0500

    trying to find the correct closest compost address

--- compost.js
+++ compost.js
@@ -13,19 +13,22 @@ const getCompostLocations = (userAddress) =>{
 
 const findNearest = (data, userAddress) => {
   let travelTime = [];
+  let saveTime = [];
   for(let i = 0; i < data.length; i++){
-    let address = data[i].location + ", " + data[i].borough + ", New York " + data[i].zip_code;
+    let address = data[i].location.split(",")[0] + ", " + data[i].borough + ", New York " + data[i].zip_code;
     const url = "http://dev.virtualearth.net/REST/V1/Routes/Driving?o=JSON&wp.0=" + userAddress.replace(" ", "%20") + "&wp.1=" + address.replace(" ", "%20") + "&routeAttributes=routePath&key=AunGgSEZ_MbKXvzPyN3B4kvqK9Ge-k8sNG3zyJ976T4DpWBAzDprClBd-Z4hA4af";
     $.ajax({
       url: url,
       type: "GET"
     }).done(function(info) {
       travelTime.push(info.resourceSets[0].resources[0].travelDuration);
+      saveTime.push(info.resourceSets[0].resources[0].travelDuration);
       if(i == data.length - 1){
         let minTime = travelTime.sort((a,b)=>a-b)[0];
+        console.log(saveTime);
         for(let j = 0; j < data.length; j++){
-          if(travelTime[j] == minTime){
-            console.log(data[j].location + ", " + data[j].borough + ", New York " + data[j].zip_code);
+          if(saveTime[j] == minTime){
+            $("body").append("<br> Travel time: " + minTime + "<br> Address:" + data[j].location + ", " + data[j].borough + ", New York " + data[j].zip_code);
           }
         }
       }
